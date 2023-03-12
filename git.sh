41baa93964ab4ff64a4a403af8d518f5d3bdbc15
Dysin <dysinqiu@163.com>
2023-03-12 21:48:19 +0800

    Dysin

--- git.sh
+++ git.sh
@@ -3,6 +3,6 @@
 git init
 git add .
 git commit -m "Dysin"
-git branch -M master
-git remote add images git@github.com:Dysin/Images.git
+#git branch -M master
+#git remote add images git@github.com:Dysin/Images.git
 git push -u images master
